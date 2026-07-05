4717c23833e1ae96f46031694691e84105710770
GabMiel <gabrielmiel50@gmail.com>
2026-07-05 23:59:51 +0800

    small progress 1

---
+++ app/src/main/java/com/example/kotlinsandbox/Playground.kt
@@ -0,0 +1,16 @@
+package com.example.kotlinsandbox
+
+/**
+ * This is a pure Kotlin playground.
+ * You can run this file directly to test Kotlin code without running the Android app.
+ * Just click the green "Run" icon next to the main function.
+ */
+fun main() {
+    println("Welcome to your Kotlin Sandbox!")
+    
+    // Example: Try a basic Kotlin feature
+    val greeting = "Hello, Kotlin!"
+    println(greeting)
+    
+    // TODO: Add your learning experiments below
+}
